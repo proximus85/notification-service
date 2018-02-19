2589dbc73a777ec97dc2e46516e0f40208eaa932
allmi <allmightypol@gmail.com>
2018-02-19 19:53:07 +0100

    adding spring boot unit test setup

---
+++ src/test/java/NotificationServiceRunnerTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.notification.NotificationServiceRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest(classes = NotificationServiceRunner.class)
+@RunWith(SpringRunner.class)
+public class NotificationServiceRunnerTest {
+
+    @Test
+    public void shouldRunSpringApplication() {
+    }
+
+}
