635e224a3dc9b470ce2347bcc354076ac26eb01d
allmi <allmightypol@gmail.com>
2018-02-19 19:37:58 +0100

    setup basic runnable spring boot application

---
+++ src/main/java/org/notification/NotificationServiceRunner.java
@@ -0,0 +1,13 @@
+package org.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceRunner.class);
+    }
+
+}
